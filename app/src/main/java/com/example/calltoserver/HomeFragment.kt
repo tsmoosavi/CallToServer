e97ac8a3bd37edff24e3182992dfbaa1ab4572b3
ts moosavi <tt.musavi@gmail.com>
2022-05-16 23:25:34 +0430

    Initial commit

---
+++ app/src/main/java/com/example/calltoserver/HomeFragment.kt
@@ -0,0 +1,28 @@
+package com.example.calltoserver
+
+import android.os.Bundle
+import android.provider.ContactsContract
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import com.example.calltoserver.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+    lateinit var binding:FragmentHomeBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = DataBindingUtil.inflate(inflater,R.layout.fragment_home,container,false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
+    }
+
+}
